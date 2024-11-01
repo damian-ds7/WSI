5e7aa291d9ddeb309d6e954af9ed9ea7d868db33
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2024-11-01 21:23:09 +0100

    removed symbols

--- cw2/constants.py
+++ cw2/constants.py
@@ -4,4 +4,3 @@ EVALUATION_LIMIT: int = 10000
 TABLE_DIR: Path = Path(__file__).parent / "tables"
 MAX_X: int = 100
 DIMENSIONALITY: int = 10
-SYMBOLS: dict[str, str] = {"sigma": "\u03c3", "mu": "\u03bc"}
