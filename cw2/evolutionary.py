99a2647860d59f2a4161c4639f97a7784fefbb3a
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2024-11-01 21:22:58 +0100

    optimized selection

--- cw2/evolutionary.py
+++ cw2/evolutionary.py
@@ -6,15 +6,25 @@ from numpy.typing import NDArray
 
 
 def selection(mu: int, values: NDArray[float]) -> NDArray[int]:
-    indices: NDArray[int] = np.zeros(mu, dtype="int64")
+    # indices: NDArray[int] = np.zeros(mu, dtype="int64")
 
-    for i in range(mu):
-        item_1, item_2 = np.random.randint(0, mu, size=2)
+    indices: NDArray[int] = np.random.randint(0, mu, (mu, 2))
 
-        if values[item_1] >= values[item_2]:
-            indices[i] = item_2
-        else:
-            indices[i] = item_1
+    selected: NDArray[int] = np.where(
+        values[indices[:, 0]] < values[indices[:, 1]],
+        indices[:, 0],
+        indices[:, 1]
+    )
+
+    return selected
+
+    # for i in range(mu):
+    #     item_1, item_2 = np.random.randint(0, mu, size=2)
+
+    #     if values[item_1] >= values[item_2]:
+    #         indices[i] = item_2
+    #     else:
+    #         indices[i] = item_1
 
     return indices
 
@@ -22,7 +32,7 @@ def selection(mu: int, values: NDArray[float]) -> NDArray[int]:
 def mutation(
     mu: int, sigma: float, population: NDArray[float]
 ) -> NDArray[NDArray[float]]:
-    masks = sigma * np.random.uniform(-1, 1, (mu, DIMENSIONALITY))
+    masks = sigma * np.random.normal(0, 1, (mu, DIMENSIONALITY))
     return (population + masks).clip(-MAX_X, MAX_X)
 
 
@@ -48,3 +58,7 @@ def evolutionary(
         optimum = temp_opt if temp_opt < optimum else optimum
 
     return optimum
+
+if __name__ == "__main__":
+    from cec2017.functions import f2
+    evolutionary(f2, 0.5, np.random.uniform(-100, 100, (20,)))
