69ad1efaba3ca747b55dbb55d9872ebbe08b5e18
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2024-11-01 21:27:50 +0100

    modified TABLE_DIR

--- cw2/constants.py
+++ cw2/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 EVALUATION_LIMIT: int = 10000
-TABLE_DIR: Path = Path(__file__).parent / "tables"
+TABLE_DIR: Path = Path(__file__).parent / "tables" / "csv"
 MAX_X: int = 100
 DIMENSIONALITY: int = 10
