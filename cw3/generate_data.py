88e967128295fefc6b70fe3ae648f28c636ad363
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2024-11-25 14:40:50 +0100

    Added function to generate depth impact data

--- cw3/generate_data.py
+++ cw3/generate_data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import numpy as np
 from checkers_stud import (
+    MINIMAX_DEPTH,
     ai_vs_ai,
     basic_ev_func,
     group_prize_ev_func,
@@ -15,7 +16,7 @@ from matplotlib import pyplot as plt
 num_workers = max(1, os.cpu_count() // 2)
 
 
-def generate_data():
+def generate_ev_function_data():
     tries = 100
     ev_functions = [
         basic_ev_func,
@@ -24,17 +25,17 @@ def generate_data():
         push_forward_ev_func,
     ]
 
-    f_count = len(ev_functions)
+    col_count = len(ev_functions)
 
     plot_path = Path(__file__).parent / "plots"
     plot_path.mkdir(exist_ok=True)
 
-    x = np.arange(f_count)
+    x = np.arange(col_count)
     categories = ["basic", "group", "opp_half", "forward"]
 
-    bottom_count = np.zeros(f_count)
-    mid_count = np.zeros(f_count)
-    top_count = np.zeros(f_count)
+    bottom_count = np.zeros(col_count)
+    mid_count = np.zeros(col_count)
+    top_count = np.zeros(col_count)
 
     for i, function in enumerate(ev_functions):
         print(f"Analyzing {function.__name__} results")
@@ -65,8 +66,55 @@ def generate_data():
 
     plt.xticks(x, categories)
     plt.savefig(plot_path / "ev_func_impact.png")
-    plt.show()
+
+
+def generate_depth_data():
+    tries = 100
+    extra_depths = [-1, 1, 2, 3]
+
+    plot_path = Path(__file__).parent / "plots"
+    plot_path.mkdir(exist_ok=True)
+
+    col_count = len(extra_depths)
+
+    bottom_count = np.zeros(col_count)
+    mid_count = np.zeros(col_count)
+    top_count = np.zeros(col_count)
+
+    x = np.arange(col_count)
+    categories = [MINIMAX_DEPTH + extra for extra in extra_depths]
+
+    for i, extra_depth in enumerate(extra_depths):
+        print(f"Analyzing results for depth={MINIMAX_DEPTH + extra_depth}")
+
+        for j in range(tries):
+            result = ai_vs_ai(basic_ev_func, extra_depth=extra_depth, concurrent=True)
+
+            if result == [True, True]:
+                mid_count[i] += 1
+            elif result[0]:
+                bottom_count[i] += 1
+            else:
+                top_count[i] += 1
+
+    plt.bar(x, bottom_count, label="Biały", color="b")
+    plt.bar(x, mid_count, bottom=bottom_count, label="Remis", color="g")
+    plt.bar(
+        x,
+        top_count,
+        bottom=bottom_count + mid_count,
+        label="Czarny",
+        color="r",
+    )
+
+    plt.xlabel("Głębokość drzewa przeszukiwań")
+    plt.ylabel("Wynik gry")
+    plt.legend()
+
+    plt.xticks(x, categories)
+    plt.savefig(plot_path / "depth_impact.png")
 
 
 if __name__ == "__main__":
-    generate_data()
+    # generate_ev_function_data()
+    generate_depth_data()
