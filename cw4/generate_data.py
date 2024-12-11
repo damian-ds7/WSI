b80b386eae3424cc37fcca74013bd51122d95ebb
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2024-12-11 16:35:42 +0100

    added gen_mistake_matrix function

--- cw4/generate_data.py
+++ cw4/generate_data.py
@@ -14,13 +14,36 @@ def calculate_accuracy(test_data: pd.DataFrame, sample: pd.DataFrame):
     return accuracy
 
 
+def gen_mistake_matrix(
+    test_data: pd.DataFrame, sample: pd.DataFrame, classes: list[str]
+):
+    mistake_matrix = np.zeros((2, 2), dtype="int64")
+
+    predicted_classes = sample[0]
+    actual_classes = test_data[0]
+
+    for predicted_class, actual_class in zip(predicted_classes, actual_classes):
+        if predicted_class == actual_class:
+            if predicted_class == classes[0]:
+                mistake_matrix[0, 0] += 1
+            else:
+                mistake_matrix[1, 1] += 1
+        else:
+            if predicted_class == classes[0]:
+                mistake_matrix[0, 1] += 1
+            else:
+                mistake_matrix[1, 0] += 1
+
+    return mistake_matrix
+
+
 def generate_data(training_part: int, testing_part: int, data_filename: str):
     data_file = CUR_DIR / data_filename
     data = pd.read_csv(data_file, header=None)
 
     split_ratio: float = training_part / (testing_part + training_part)
 
-    indices = np.array(list(range(0, len(data))))
+    indices = np.arange(len(data))
     rng = np.random.default_rng()
     train_indices = rng.choice(indices, int(len(data) * split_ratio), replace=False)
     test_indices = np.setdiff1d(indices, train_indices)
@@ -35,13 +58,19 @@ def generate_data(training_part: int, testing_part: int, data_filename: str):
 
     sample = id3.predict(sample)
 
-    print(calculate_accuracy(test_data, sample))
+    return calculate_accuracy(test_data, sample)
+    print(gen_mistake_matrix(test_data, sample, np.unique(sample[0])))
 
 
 if __name__ == "__main__":
+    total = 0
 
     for i in range(100):
-        generate_data(3, 2, "breast-cancer.data")
+        x = generate_data(3, 2, "breast-cancer.data")
+        total += x
+        print(x)
+
+    print(total / 100)
 
     # id3 = Id3()
     # id3.train(data, 0)
